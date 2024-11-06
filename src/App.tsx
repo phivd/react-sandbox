2eb8aed81c903a7935e2630cc755f644bfa3bb3c
Philvdu <phil.videau@gmail.com>
2024-11-06 17:29:12 -0500

    redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import WeatherPage from './pages/WeatherPage';
 import "./App.css"
 
 function App() {
+  const [activeLink, setActiveLink] = useState('');
+
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link);
+  };
+
   return (
     <BrowserRouter>
       <div className='App-header'>
         <nav>
-          <Link to="/weather" className='App-link'>Weather</Link>
-          <Link to="/login" className='App-link'>Login</Link>
+          <Link to="/weather" 
+          className={`App-link ${activeLink === 'weather' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('weather')}>Weather</Link>
+          <Link to="/login" 
+          className={`App-link ${activeLink === 'login' ? 'active' : ''}`}
+          onClick={() => handleLinkClick('login')}>Login</Link>
         </nav>
       </div>
       <div className='App-body'>
--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -13,14 +13,27 @@ const HomePage: React.FC = () => {
     } catch {}
   };
 
+  const enterKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch()
+    }
+  };
+
   return (
     <div>
-      <input type="text" placeholder="Enter the city" value={city} onChange={(e) => setCity(e.target.value)}/>
-      <button onClick={handleSearch}>Get weather</button>
+      <div className='weather-search'>
+        <input type="text" 
+        placeholder="Enter the city" 
+        className="weather-search-input-field" 
+        value={city} 
+        onKeyDown={enterKey} 
+        onChange={(e) => setCity(e.target.value)}/>
+        <button className="weather-search-button" onClick={handleSearch}>Get weather</button>
+      </div>
       {weatherData && (
         <div>
           <p>Weather in {weatherData.name}:</p>
-          <div className='Weather'>
+          <div className='weather-body'>
             <p>Temperature: {weatherData.main.temp}°C</p>
             {/* TODO: autres informations meteo */}
           </div>
